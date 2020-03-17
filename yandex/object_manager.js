b4c838c320b730d9b67c999abec2a95e85cded83
Ochirov Aiur <ochirovaur2@mail.ru>
2020-03-17 19:28:06 +0300

    Add files via upload

---
+++ yandex/object_manager.js
@@ -0,0 +1,31 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [54.89794,86.990382],
+            zoom: 2,
+            controls: ['zoomControl', 'fullscreenControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            // clusterize: true,
+            // // ObjectManager принимает те же опции, что и кластеризатор.
+            // gridSize: 32,
+            // clusterDisableClickZoom: true
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#redIcon');
+    // objectManager.clusters.options.set('preset', 'islands#invertedRedClusterIcons');
+    myMap.geoObjects.add(objectManager);
+    myMap.behaviors.disable('scrollZoom');
+    $.ajax({
+        url: "data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+}
